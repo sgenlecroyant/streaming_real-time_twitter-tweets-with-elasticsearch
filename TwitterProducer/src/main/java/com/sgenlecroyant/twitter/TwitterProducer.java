0a6fd2b25c5b52e547d950425f17721c24992465
sgenlecroyant <sgenlecroyant@gmail.com>
2022-10-25 15:52:22 +0200

    done testing, let's refactor the code now

--- TwitterProducer/src/main/java/com/sgenlecroyant/twitter/TwitterProducer.java
+++ TwitterProducer/src/main/java/com/sgenlecroyant/twitter/TwitterProducer.java
@@ -16,9 +16,8 @@ import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
-import com.twitter.hbc.core.endpoint.StreamingEndpoint;
+import com.twitter.hbc.core.processor.HosebirdMessageProcessor;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
-import com.twitter.hbc.httpclient.BasicClient;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 
@@ -26,17 +25,22 @@ import com.twitter.hbc.httpclient.auth.OAuth1;
 public class TwitterProducer {
 	private final static Logger LOGGER = LoggerFactory.getLogger(TwitterProducer.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(TwitterProducer.class, args);
 
 		BlockingQueue<String> messages = new ArrayBlockingQueue<>(1000);
 
 		Hosts hosts = new HttpHosts(Constants.STREAM_HOST);
 
-		String consumerKey = "hYYGyQV241Gt7sb2rKLrNcBgy";
-		String consumerSecret = "pso3ZO6N7TjgOOehax241QOIAvo4rnryuXWkVOPEacIXjzUvlV";
-		String token = "990132492949704705-WiVkaj9zhtQDo98uq2NqccJt3HxrH64";
-		String tokenSecret = "UDyAkL38mQLwhkUvDMyN7e996M6BRdY90rnoTHh1cBOoJ";
+//		String consumerKey = "hYYGyQV241Gt7sb2rKLrNcBgy";
+//		String consumerSecret = "pso3ZO6N7TjgOOehax241QOIAvo4rnryuXWkVOPEacIXjzUvlV";
+//		String token = "990132492949704705-WiVkaj9zhtQDo98uq2NqccJt3HxrH64";
+//		String tokenSecret = "UDyAkL38mQLwhkUvDMyN7e996M6BRdY90rnoTHh1cBOoJ";
+
+		String consumerKey = "QkVZbzd1tj5yrlWw5nDBMnWAC";
+		String consumerSecret = "zNr5E8Y76gcq2eiWlYlrot0rfbIef3vhlyiAHc7VjWMu8kbspc";
+		String token = "990132492949704705-O3hXe4BfUELS33CBpuerKhOjLunp5Yr";
+		String tokenSecret = "9znP4Au7uJv4oGGMLaUXmGDwI8oArs9sEU5y4Ij3uWuvV";
 
 		Authentication authentication = new OAuth1(consumerKey, consumerSecret, token, tokenSecret);
 
@@ -44,22 +48,27 @@ public class TwitterProducer {
 		List<String> terms = Arrays.asList("bitcoin", "chelsea", "trump", "burundi");
 		statusesFilterEndpoint.trackTerms(terms);
 
+		HosebirdMessageProcessor messageProcessor = new StringDelimitedProcessor(messages);
 		Client client = new ClientBuilder().authentication(authentication).endpoint(statusesFilterEndpoint).hosts(hosts)
-				.processor(new StringDelimitedProcessor(messages)).build();
+				.processor(messageProcessor).build();
 
+		System.out.println("before connecting ...");
 		client.connect();
+		System.out.println("after connecting ...");
+
 		String message = null;
 
 		while (!client.isDone()) {
 			if (message == null) {
 				try {
 					message = messages.take();
+					LOGGER.info(message);
+					message = null;
 				} catch (InterruptedException e) {
 					LOGGER.error("Encountered an error: " + e);
 				}
 			}
 
-//			client.
 		}
 
 	}
